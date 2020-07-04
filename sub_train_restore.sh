f0b35df887bda57a332d9e77a0388f0812c02b36
Jonatan Kr <jonatan.kronander@gmail.com>
2020-07-04 11:12:47 +0200

    del logfiles

--- sub_train_restore.sh
+++ sub_train_restore.sh
@@ -8,4 +8,4 @@
 #$ -q gpu.24h.q
 source /scratch_net/biwidl214/jonatank/anaconda3/etc/profile.d/conda.sh
 conda activate JKMT
-python -u train_restore_MAP_NN.py --name 3subj_1e1_10steps_4fch3sh_iterAug_3_ --config conf/conf_nn.yaml --subjs 200 --K_actf 0
+python -u train_restore_MAP_NN.py --name 3subj_1e1_10steps_4fch3sh_iterAug_EX100_3_ --config conf/conf_nn.yaml --subjs 200 --K_actf 0
